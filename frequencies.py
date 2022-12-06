aecba46ba6c7f71aab71c80159be679e2ff628f3
justthatp <bolvinrain@gmail.com>
2022-12-06 21:26:23 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,17 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(my_list):
+def frequencies(items):
     frequencies = {}
     # Your code goes here
-    for items in my_list:
-        if(isinstance(items,str)):
-         frequencies[items] = my_list.count(items)
-        else:
-         a= str(items)
-         frequencies[a] = my_list.count(items)
-        
 
+    # Your code goes here
+   
+    for inputs in items:
+        frequencies[str(inputs)] = frequencies.get(str(inputs), 0) + 1
+   
+    return frequencies
      
    # for key, value in frequencies.items():
      #   print ("% d : % d"%(key, value))
